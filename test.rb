35b9d7e72723f31f476e51d35a3a93f4963a8bec
Baldur Gudbjornsson <baldur.gudbjornsson@gmail.com>
2014-09-05 15:04:25 -0400

    letting nokogiri read the file

--- test.rb
+++ test.rb
@@ -6,9 +6,11 @@ require 'find'
 Find.find('.') do |path|
   if !FileTest.directory?(path) && File.extname(path) == ".xml"
     begin
-      bad_doc = Nokogiri::XML(File.read(path)) { |config| 
+      file = File.open(path)
+      bad_doc = Nokogiri::XML(file) { |config| 
         config.options = Nokogiri::XML::ParseOptions::STRICT 
       }
+      file.close
     rescue Nokogiri::XML::SyntaxError => e
       puts e
       exit 1
